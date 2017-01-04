d61bcc169ae08b067482277aa7f8edb3758ebf83
dzrust <danielzrust@gmail.com>
2017-01-03 18:13:05 -0700

    Worked on character scripts and started laying out structurfe for basic mobbs and towers

--- Assets/Scripts/CharacterScripts.cs
+++ Assets/Scripts/CharacterScripts.cs
@@ -3,23 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using MOBA.ExtClasses;
 
-public class CharacterScripts : MonoBehaviour
+public class CharacterScripts : GeneralCharacter
 {
-
-    public GeneralCharacter characterAttributes;
-
-    public Transform playerObject { get; set; }
-
-    public int aPower;
-
     private bool isActivatingAbility;
     private bool characterTimer;
+    private float zCameraOffset;
 
     #region Unity Functions
     // Use this for initialization
     void Start()
     {
-
+        SetTargetPosition(new Vector3(211, 2, 226));
+        zCameraOffset = playerCamera.transform.position.z - playerTransform.position.z;
     }
 
     // Update is called once per frame
@@ -27,79 +22,109 @@ public class CharacterScripts : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            print("Key pressed");
-            characterAttributes.Move(Input.mousePosition);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Move(ray);
         }
+        playerTransform.position = Vector3.MoveTowards(playerTransform.position, GetTargetPosition(), Time.deltaTime * movementSpeed);
+        playerCamera.transform.position = Vector3.MoveTowards(playerCamera.transform.position, new Vector3() { x = GetTargetPosition().x, z = GetTargetPosition().z+ zCameraOffset, y = playerCamera.transform.position.y }, Time.deltaTime * movementSpeed);
     }
     #endregion
 
 
 }
-
-public class GeneralCharacter : ICharacter
+namespace MOBA.ExtClasses
 {
-
-    public float health { get; set; }
-    public float aPowerk;
-    public float attack { get; set; }
-    public float defense { get; set; }
-    public float agility { get; set; }
-    public float movementSpeed { get; set; }
-    public float attackSpeed { get; set; }
-    public float mana { get; set; }
-    public float abilityPower { get; set; }
-    public float physicalPenetration { get; set; }
-    public float magicalPenetration { get; set; }
-    public List<Ability> abilities { get; set; }
-    public List<Equippable> equipment { get; set; }
-    public int money { get; set; }
-
-    public Animation attackAnimation { get; set; }
-    public Animation walkAnimation { get; set; }
-    public Animation idleAnimation { get; set; }
-    public Animation deathAnimation { get; set; }
-    public Animation lifeAnimation { get; set; }
-    #region Other Functions
-
-    public void Move(Vector3 movement)
-    {
-        //transform.position = Vector3.MoveTowards(transform.position, movement, Time.deltaTime * 5);
-    }
-
-    public void DecreaseHealth(float attackedAmmount)
+    public class GeneralCharacter : MonoBehaviour
     {
 
+        public float health;
+        public float aPowerk;
+        public float attack;
+        public float defense;
+        public float agility;
+        public float movementSpeed;
+        public float attackSpeed;
+        public float mana;
+        public float abilityPower;
+        public float physicalPenetration;
+        public float magicalPenetration;
+        public List<Ability> abilities;
+        public List<Equippable> equipment;
+        public int money;
+
+        public Animation attackAnimation;
+        public Animation walkAnimation;
+        public Animation idleAnimation;
+        public Animation deathAnimation;
+        public Animation lifeAnimation;
+
+        public Transform playerTransform;
+        public Camera playerCamera;
+        private Vector3 targetPosition;
+        #region Other Functions
+
+
+        public Vector3 GetTargetPosition()
+        {
+            return targetPosition;
+        }
+
+
+        public void SetTargetPosition(Vector3 newPosition)
+        {
+            targetPosition = newPosition;
+        }
+
+        public void Move(Ray ray)
+        {
+            Plane playerPlane = new Plane(Vector3.up, playerTransform.position);
+            float hitdist = 0.0f;
+
+            if (playerPlane.Raycast(ray, out hitdist))
+            {
+
+                var targetPoint = ray.GetPoint(hitdist);
+                targetPosition = ray.GetPoint(hitdist);
+                var targetRotation = Quaternion.LookRotation(targetPoint - playerTransform.position);
+                playerTransform.rotation = targetRotation;
+            }
+        }
+
+        public void DecreaseHealth(float attackedAmmount)
+        {
+
+        }
+
+        public void HealHealth(float healAmmount)
+        {
+
+        }
+
+        public void IncreaseHealth(float increaseAmmount)
+        {
+
+        }
+
+        public void Attack(GameObject target)
+        {
+
+        }
+
+        public void Equip(GameObject equipable)
+        {
+
+        }
+
+        public void CastTargetSelectAbility(GameObject target)
+        {
+
+        }
+
+        public void CastAreaSelectAbility(Vector3 location)
+        {
+
+        }
+
+        #endregion
     }
-
-    public void HealHealth(float healAmmount)
-    {
-
-    }
-
-    public void IncreaseHealth(float increaseAmmount)
-    {
-
-    }
-
-    public void Attack(GameObject target)
-    {
-
-    }
-
-    public void Equip(GameObject equipable)
-    {
-
-    }
-
-    public void CastTargetSelectAbility(GameObject target)
-    {
-
-    }
-
-    public void CastAreaSelectAbility(Vector3 location)
-    {
-
-    }
-
-    #endregion
 }
