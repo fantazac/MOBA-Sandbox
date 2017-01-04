a5d94899b3c0133309f2975922f2e5bf8a115359
Alexandre <alex>
2017-01-03 22:39:34 -0500

    Click to move working in scene Alex

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject menuCamera;
+
+    private void Start()
+    {
+        Connect();
+    }
+
+    private void Connect()
+    {
+        PhotonNetwork.ConnectUsingSettings("MOBA v1.0.0");
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+    }
+
+    private void OnJoinedLobby()
+    {
+        Debug.Log("OnJoinedLobby");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    private void OnPhotonRandomJoinFailed()
+    {
+        Debug.Log("OnPhotonRandomJoinFailed");
+        PhotonNetwork.CreateRoom(null);
+    }
+
+    private void OnJoinedRoom()
+    {
+        Debug.Log("OnJoinedRoom");
+
+        SpawnMyPlayer();
+    }
+
+    private void SpawnMyPlayer()
+    {
+        PhotonNetwork.Instantiate("Player", Vector3.up, new Quaternion(), 0);
+        menuCamera.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject moveTo;
+
+    private Rigidbody _rigidbody;
+    private TerrainCollider terrainCollider;
+    private Camera childCamera;
+
+    private GameObject moveToPoint;
+
+    private Vector3 halfHeight;
+
+    private bool isMoving = false;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        terrainCollider = GameObject.Find("Terrain").GetComponent<TerrainCollider>();
+        childCamera = GetComponentInChildren<Camera>();
+        halfHeight = Vector3.up * transform.localScale.y * 0.5f;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+            Ray ray = childCamera.ScreenPointToRay(Input.mousePosition);
+            if (terrainCollider.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                Destroy(moveToPoint);
+                moveToPoint = (GameObject)Instantiate(moveTo, hit.point + halfHeight, new Quaternion());
+                StopAllCoroutines();
+                StartCoroutine(MoveTowardsWherePlayerClicked(hit.point + halfHeight));
+            }
+        }
+    }
+
+    private IEnumerator MoveTowardsWherePlayerClicked(Vector3 wherePlayerClickedToMove)
+    {
+        while(transform.position != wherePlayerClickedToMove)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, wherePlayerClickedToMove, Time.deltaTime * 25);
+
+            yield return null;
+        }
+        Destroy(moveToPoint);
+    }
+}
