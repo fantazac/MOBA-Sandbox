76a69b9b57177401c7df2d716059897c548bdd67
Alexandre <alex>
2017-01-09 21:20:31 -0500

    EzrealW

---
+++ Assets/Scripts/ChampionAbilities/Ezreal/EzrealW.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EzrealW : PlayerSkill
+{
+
+    [SerializeField]
+    private GameObject projectile;
+
+    private float range = 22;
+    private float speed = 35;
+
+    RaycastHit hit;
+
+    protected override void Start()
+    {
+        skillId = 1;
+        delayCastTime = new WaitForSeconds(castTime);
+        base.Start();
+    }
+
+    [PunRPC]
+    protected void UseEzrealWFromServer(Vector3 mousePositionOnCast)
+    {
+        skillActive = true;
+        StartCoroutine(SkillEffectWithCastTime(mousePositionOnCast));
+    }
+
+    public override void ActivateSkill()
+    {
+        playerMovement.PhotonView.RPC("UseEzrealWFromServer", PhotonTargets.All, hit.point + playerMovement.halfHeight);
+    }
+
+    public override bool CanUseSkill(Vector3 mousePosition)
+    {
+        return playerMovement.terrainCollider.Raycast(playerMovement.GetRay(mousePosition), out hit, Mathf.Infinity) && playerMovement.CanCastSpell(this);
+    }
+
+    protected override IEnumerator SkillEffectWithCastTime(Vector3 mousePositionOnTerrain)
+    {
+        playerMovement.PlayerOrientation.RotatePlayerInstantly(mousePositionOnTerrain);
+
+        yield return delayCastTime;
+
+        GameObject projectileToShoot = (GameObject)Instantiate(projectile, transform.position, transform.rotation);
+        projectileToShoot.GetComponent<ProjectileMovement>().ShootProjectile(speed, range);
+
+        SkillDone();
+    }
+
+}
