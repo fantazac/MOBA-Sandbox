db79f505bb34f21463b3531d954537b9643d0562
Alexandre <alex>
2017-01-13 22:40:37 -0500

    Camera moves when mouse is on edges

--- Assets/Scripts/Camera/CameraManager.cs
+++ Assets/Scripts/Camera/CameraManager.cs
@@ -4,6 +4,15 @@ using System;
 
 public class CameraManager : MonoBehaviour
 {
+    [SerializeField]
+    private int distanceFromBorderToStartMoving = 60;
+
+    [SerializeField]
+    private int speed = 28;
+
+    private int screenWidth;
+    private int screenHeight;
+
     private Player player;
 
     private bool cameraLockedOnPlayer = true;
@@ -11,27 +20,53 @@ public class CameraManager : MonoBehaviour
 
     private Vector3 initialPosition;
 
+    private Vector3 mousePositionOnFrame;
+
     private void Start()
     {
         player = transform.parent.GetChild(3).gameObject.GetComponent<Player>();
-        if (player != null)
+        player.PlayerInput.OnPressedY += SetCameraLock;
+        player.PlayerInput.OnPressedSpace += SetCameraOnPlayer;
+        player.PlayerInput.OnReleasedSpace += SetCameraFree;
+        
+        screenWidth = Screen.width;
+        screenHeight = Screen.height;
+
+        initialPosition = transform.position;
+        transform.position += player.transform.position;
+        player.PlayerMovement.PlayerMoved += FollowPlayer;
+    }
+
+    private void Update()
+    {
+        if (!CameraShouldFollowPlayer())
         {
-            player.PlayerInput.OnPressedY += SetCameraLock;
-            player.PlayerInput.OnPressedSpace += SetCameraOnPlayer;
-            player.PlayerInput.OnReleasedSpace += SetCameraFree;
-            initialPosition = transform.position;
-            transform.position += player.transform.position;
-            player.GetComponent<PlayerMovement>().PlayerMoved += FollowPlayer;
-        }
-        else
-        {
-            throw new Exception("Player set to null reference");
+            mousePositionOnFrame = Input.mousePosition;
+
+            if(mousePositionOnFrame.x > (screenWidth - distanceFromBorderToStartMoving))
+            {
+                transform.position += Vector3.right * speed * Time.deltaTime;
+            }
+            else if (mousePositionOnFrame.x < distanceFromBorderToStartMoving)
+            {
+                transform.position += Vector3.left * speed * Time.deltaTime;
+            }
+
+            if (mousePositionOnFrame.y > (screenHeight - distanceFromBorderToStartMoving))
+            {
+                transform.position += Vector3.forward * speed * Time.deltaTime;
+            }
+            else if (mousePositionOnFrame.y < distanceFromBorderToStartMoving)
+            {
+                transform.position += Vector3.back * speed * Time.deltaTime;
+            }
         }
     }
 
     private void SetCameraOnPlayer()
     {
         cameraFollowsPlayer = true;
+        FollowPlayer();
     }
 
     private void SetCameraFree()
@@ -42,13 +77,19 @@ public class CameraManager : MonoBehaviour
     private void SetCameraLock()
     {
         cameraLockedOnPlayer = !cameraLockedOnPlayer;
+        FollowPlayer();
     }
 
     private void FollowPlayer()
     {
-        if (cameraLockedOnPlayer || cameraFollowsPlayer)
+        if (CameraShouldFollowPlayer())
         {
             transform.position = player.transform.position + initialPosition;
         }
+    }
+
+    private bool CameraShouldFollowPlayer()
+    {
+        return cameraLockedOnPlayer || cameraFollowsPlayer;
     }
 }
