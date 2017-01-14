7e4e52825eddf333ac1e848d88ad815929cfbd30
Alexandre <alex>
2017-01-14 02:43:41 -0500

    Ezreal R range, debug

--- Assets/Scripts/ChampionAbilities/Ezreal/EzrealR.cs
+++ Assets/Scripts/ChampionAbilities/Ezreal/EzrealR.cs
@@ -5,7 +5,7 @@ public class EzrealR : Skillshot
 {
     protected override void Start()
     {
-        range = 150;
+        range = 250;
         speed = 36;
         delayCastTime = new WaitForSeconds(castTime);
         base.Start();
